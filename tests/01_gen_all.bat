d7aae0c7981ecfcc765dcb811cf31bf57d9b0f9f
Jimit04 <jimitvyas1@gmail.com>
2026-03-01 16:43:59 +0530

    removed outdated files

--- tests/01_gen_all.bat
+++
@@ -1,6 +0,0 @@
-uv run main.py --generate --samples 500 --sampling "random"
-uv run main.py --generate --samples 500 --sampling "lhs"
-uv run main.py --generate --samples 500 --sampling "sobol"
-uv run main.py --generate --samples 500 --sampling "taguchi"
-
-
--- tests/02_gcn_all.bat
+++
@@ -1,6 +0,0 @@
-uv run main.py --train --model "gcn" --sampling "random"
-uv run main.py --train --model "gcn" --sampling "lhs"
-uv run main.py --train --model "gcn" --sampling "sobol"
-uv run main.py --train --model "gcn" --sampling "taguchi"
-
-
